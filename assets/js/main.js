a22f292e14fbf2639d74cdb7f29c4a632a4f620d
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-25 16:01:08 +0100

    feat: add img bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,9 @@ createApp ({
     data () {
         return {
             message: "Barbero! Barbero! Illuminaci il sentiero!",
-            barbero: "text-center text-success"
+            barbero: "text-center text-success",
+            imgBarbero: "assets/img/Alessandro_Barbero_Dominus.jpg",
+            dominus: "position-absolute top-50 start-50 translate-middle rounded-5",
         }
     }
 }).mount ('#app')
